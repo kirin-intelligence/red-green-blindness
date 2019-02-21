5d4e9b6c4aa55300ce2e0983081e696b5594baad
wscjxky <619677632@qq.com>
2019-02-21 18:09:57 +0800

    i

--- spider.py
+++ spider.py
@@ -1,27 +1,27 @@
 # https://lbs.amap.com/api/webservice/guide/api/staticmaps/
 import requests
-
-filename = 'data/jiaot'
+import time
+filename = 'fuckdu'
 size = 600
 # traffic = 1  # 是否要实况图
 for i in range(2):
     data = requests.get('https://restapi.amap.com/v3/staticmap?&'
-                        'zoom=17&size=' + str(size) + '*' + str(size) +
-                        '&location=116.346942,39.954295&'
+                        'zoom=15&size=' + str(size) + '*' + str(size) +
+                        '&location=116.441119,39.901121&'
                         'scale=2&'
                         'traffic=' + str(i) +
                         '&key=2be4c36d53e74e0c585326d62d6fe6e3')
     with open(filename + str(i) + '.png', 'wb') as file:
         file.write(data.content)
-
-size = 600
-filename = 'test'
-for i in range(2):
-    data = requests.get('https://restapi.amap.com/v3/staticmap?&'
-                        'zoom=17&size=' + str(size) + '*' + str(size) +
-                        '&location=116.352615,39.908664&'
-                        'scale=2&'
-                        'traffic=' + str(i) +
-                        '&key=2be4c36d53e74e0c585326d62d6fe6e3')
-    with open(filename + str(i) + '.png', 'wb') as file:
-        file.write(data.content)
+#
+# size = 600
+# filename = 'test'
+# for i in range(2):
+#     data = requests.get('https://restapi.amap.com/v3/staticmap?&'
+#                         'zoom=17&size=' + str(size) + '*' + str(size) +
+#                         '&location=116.352615,39.908664&'
+#                         'scale=2&'
+#                         'traffic=' + str(i) +
+#                         '&key=2be4c36d53e74e0c585326d62d6fe6e3')
+#     with open(filename + str(i) + '.png', 'wb') as file:
+#         file.write(data.content)
