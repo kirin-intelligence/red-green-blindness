bd8164ba7ec4ff2738f6c8800e3d6e5e2ce78b3c
wscjxky <619677632@qq.com>
2019-02-24 15:47:05 +0800

    i

---
+++ config.py
@@ -0,0 +1,14 @@
+left_point = [116.204495, 39.994064]
+right_point = [116.316193, 39.892346]
+left_point = [116.224495, 39.994064]
+right_point = [116.306193, 39.892346]
+x1 = left_point[0]
+y1 = left_point[1]
+x_step = 0.0124 * 1.7
+x_count = 0.1 // x_step
+y_step = 0.009 * 1.8
+y_count = 0.1 // y_step
+size = 1200
+x_count = 20
+
+UNIT_SIZE = 2048
