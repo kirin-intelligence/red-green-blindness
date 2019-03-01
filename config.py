460708e7e56f820ca86346d3a97c91e4811ba6c3
KirinNg <kirinng0709@gmail.com>
2019-03-02 06:38:29 +0800

    Sdf

--- config.py
+++ config.py
@@ -1,24 +1,4 @@
-<<<<<<< HEAD
-left_point = [116.204495, 39.994064]
-right_point = [116.316193, 39.892346]
 
-left_point = [116.2554495, 39.994064]
-
-# left_point = [116.300892,39.967359]
-# right_point = [116.306193, 39.892346]
-
-
-x1 = left_point[0]
-y1 = left_point[1]
-x_step = 0.0124
-x_count = 0.1 // x_step
-y_step = 0.009
-y_count = 0.1 // y_step
-size = 600
-x_count = 20
-
-UNIT_SIZE = 2048
-=======
 left_point = [116.204495, 39.994064]
 right_point = [116.316193, 39.892346]
 #left_point = [116.224495, 39.984064]
@@ -37,4 +17,3 @@ size = 1200
 x_count = 20
 
 UNIT_SIZE = 2048
->>>>>>> 98b7b1b46cc629c56bf544919774b29bcc1aaf41
