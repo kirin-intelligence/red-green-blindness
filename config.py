a9eef322502b14db4762d1c07470c4b712d63deb
wscjxky <619677632@qq.com>
2019-03-02 15:25:06 +0800

    finish

--- config.py
+++ config.py
@@ -5,15 +5,15 @@ right_point = [116.316193, 39.892346]
 
 # left_point = [116.300892,39.967359]
 # right_point = [116.306193, 39.892346]
-
-
 x1 = left_point[0]
 y1 = left_point[1]
-x_step = 0.0124 * 1.7
+x_step = 0.0124
 x_count = 0.1 // x_step
-y_step = 0.009 * 1.8
+y_step = 0.009
 y_count = 0.1 // y_step
 size = 1200
-x_count = 20
-
-UNIT_SIZE = 2048
+x_count = 15
+UNIT_SIZE = 1200
+from redis import StrictRedis, ConnectionPool
+pool = ConnectionPool(host='123.56.19.49', password='wscjxky123', port=6379, db=1)
+redis = StrictRedis(connection_pool=pool)
--- get_line.py
+++ get_line.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import numpy as np
 import cv2
 
@@ -82,12 +81,8 @@ def find_point(filename,GPS_Center):
             cv2.circle(res, tuple(p[0: 2]), 5, (0, 0, 220), 1)
         if p[2] == 2:
             cv2.circle(res, tuple(p[0: 2]), 5, (13, 13, 138), 1)
-        print(p)
 
-        break
-
-    cv2.imwrite(filename+'.png',res)
-    print(filename)
+    # cv2.imwrite(filename+'.png',res)
     # cv2.imshow('Result1', res)
     # cv2.imshow('Result2', g)
     #
@@ -98,6 +93,7 @@ def find_point(filename,GPS_Center):
     def la_le_point(plst, center):
         px = 0.0124/(1200 - 44)
         py = 0.009/(1200 - 104)
+        # 44, 104
         zero_point = [center[0] - 600*px, center[1] - 600*py]
         la_le_p = []
         for point in plst:
@@ -110,81 +106,3 @@ def find_point(filename,GPS_Center):
 
 # line_graph, g_graph, y_graph, r_graph, dr_graph = get_line(img)
 # point_in_img = find_point(line_graph, g_graph, y_graph, r_graph, dr_graph, None)
-=======
-import numpy as np
-import cv2
-
-
-color_dic = {'RED_l': np.array([0, 0, 220]), 'RED_h': np.array([0, 0, 222]),
-             'YELLO_l': np.array([0, 202, 253]), 'YELLO_h': np.array([0, 204, 255]),
-             'GREEN_l': np.array([0, 175, 49]), 'GREEN_h': np.array([0, 177, 51]),
-             'DEEPRED_l': np.array([13, 13, 138]), 'DEEPRED_h': np.array([13, 13, 140]), }
-
-
-def get_line(frame):
-    # get mask
-    r = cv2.inRange(frame, color_dic['RED_l'], color_dic['RED_h'])
-    y = cv2.inRange(frame, color_dic['YELLO_l'], color_dic['YELLO_h'])
-    g = cv2.inRange(frame, color_dic['GREEN_l'], color_dic['GREEN_h'])
-    dr = cv2.inRange(frame, color_dic['DEEPRED_l'], color_dic['DEEPRED_h'])
-    mask = r + y + g + dr
-    # mask = g
-
-    res = cv2.bitwise_and(frame, frame, mask=mask)
-    return res, g, y, r, dr
-
-
-def find_point(filename):
-    img = cv2.imread(filename)
-    res, g, y, r, dr=get_line(img)
-    # point -> List: OpenCv 的点坐标是反着的 (y, x)
-    dst = cv2.cornerHarris(g, 7, 9, 0.04)
-    dst = cv2.dilate(dst, None)
-    g[dst < 0.01 * dst.max()] = 0
-
-    dst = cv2.cornerHarris(y, 7, 9, 0.04)
-    dst = cv2.dilate(dst, None)
-    y[dst < 0.01 * dst.max()] = 0
-
-    dst = cv2.cornerHarris(r, 7, 9, 0.04)
-    dst = cv2.dilate(dst, None)
-    r[dst < 0.01 * dst.max()] = 0
-
-    dst = cv2.cornerHarris(dr, 7, 9, 0.04)
-    dst = cv2.dilate(dst, None)
-    dr[dst < 0.01 * dst.max()] = 0
-
-    def have(pix):
-        if np.sum(pix) > 0:
-            return 1
-        else:
-            return 0
-    point = []
-
-    step = 10
-    l_step = res.shape[0] // step
-    w_step = res.shape[1] // step
-    for i in range(l_step):
-        for j in range(w_step):
-            status = have(g[step * i:step * (i + 1), step * j:step * (j + 1)]) + have(
-                y[step * i:step * (i + 1), step * j:step * (j + 1)]) + have(
-                r[step * i:step * (i + 1), step * j:step * (j + 1)]) + have(
-                dr[step * i:step * (i + 1), step * j:step * (j + 1)])
-            if status >= 2:
-                point.append([int(step * (j + 0.5)), int(step * (i + 0.5))])
-
-    # for p in point:
-    #     cv2.circle(res, tuple(p), 5, (255, 255, 255), 1)
-    # cv2.imwrite(filename, res)
-    return point
-    # cv2.imshow('Result1', res)
-    # cv2.imshow('Result2', g)
-
-    # cv2.waitKey(0)
-    # if cv2.waitKey(0) & 0xff == 27:
-    #     cv2.destroyAllWindows()
-    # print(point)
-    # return point
-
-# find_point('images/jiaot_2019-02-22-10_54_51.png')
->>>>>>> 98b7b1b46cc629c56bf544919774b29bcc1aaf41
