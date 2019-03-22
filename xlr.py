6efa831ec66f510a5e67fc107b4b6a6604b51117
wscjxky123 <619677632@qq.com>
2019-03-22 10:31:06 +0800

    i

--- xlr.py
+++ xlr.py
@@ -1,33 +1,41 @@
+import json
+
 import xlrd
-readbook = xlrd.open_workbook(r'Book1.xlsx')
+
+readbook = xlrd.open_workbook(r'/home/kaiyuan_xu/Downloads/evening.xlsx')
 sheet = readbook.sheet_by_index(0)
 nrows = sheet.nrows
 ncols = sheet.ncols
-arr=[]
-arr1=[]
-for i in range(3,nrows):
-    start = sheet.cell_value(i,2)
-    end = sheet.cell_value(i, 6)
-    start_place=sheet.cell_value(i,0)
-    end_place=sheet.cell_value(i,4)
-    minute=0
-    type=sheet.cell_value(i,9)
-    length=sheet.cell_value(i,8)
-    print(start_place,end_place,type,length)
-    lng=float(start.split(',')[0])
-    lat=float(start.split(',')[1])
-    lng1=float(end.split(',')[0])
-    lat1=float(end.split(',')[1])
+arr = []
+import requests
+
+arr1 = []
+for i in range(1, nrows):
+    start = sheet.cell_value(i, 0)
+    end = sheet.cell_value(i, 1)
+    type = sheet.cell_value(i, 2)
+    lng = float(start.split(',')[0])
+    lat = float(start.split(',')[1])
+    lng1 = float(end.split(',')[0])
+    lat1 = float(end.split(',')[1])
+    url = 'https://restapi.amap.com/v3/geocode/regeo?key=2be4c36d53e74e0c585326d62d6fe6e3&location=%s,%s' \
+          '&poitype=&radius=1000&extensions=base&batch=false&roadlevel=0'%(lng,lat)
+    data=json.loads(requests.get(url).text)
+    start_place=(data['regeocode']['formatted_address'])
+    url = 'https://restapi.amap.com/v3/geocode/regeo?key=2be4c36d53e74e0c585326d62d6fe6e3&location=%s,%s' \
+          '&poitype=&radius=1000&extensions=base&batch=false&roadlevel=0'%(lng1,lat1)
+    data=json.loads(requests.get(url).text)
+    end_place=(data['regeocode']['formatted_address'])
     arr.append({
-        'point':[lng,lat],
-        'desc':start_place,
-        'type':'yellow' if type =="橙色" else 'red',
-        'length':length
+        'point': [lng, lat],
+        'type': 'darkorange' if type == "橙色" else 'red',
+        'desc':start_place
     })
     arr1.append({
-        'point':[lng1,lat1],
-        'desc':end_place,
+        'point': [lng1, lat1],
+        'desc':end_place
     })
-print(arr)
+print("var positions1=" + str(arr))
+print("var positions2=" + str(arr1))
 print(len(arr))
 print(len(arr1))
