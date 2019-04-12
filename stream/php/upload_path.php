f1bea43e72e1b2c4fc94645a206907d43903ff8a
wscjxky <619677632@qq.com>
2019-04-12 13:16:15 +0800

    i

--- stream/php/upload_path.php
+++ stream/php/upload_path.php
@@ -12,17 +12,29 @@ $redis = new Redis();
 
 $redis->connect('123.56.19.49', 6379); //连接Redis
 $redis->auth('wscjxky123'); //密码验证
+$redis->select(10);
 
+$opera= $_GET['opera'];
 $day = $_GET['day'];
+$no = $_GET['no'];
+$hname = $day . ':' . $no;
+if ($opera=='del'){
+    $res = $redis->del($hname);
+    exit(1);
+}
 $paths = $_GET['paths'];
 $type = $_GET['type'];
-$no = $_GET['no'];
 $distance = $_GET['distance'];
-$redis->select(8);
-$hname = $day . ':' . $no;
+
+
 //$res=$redis->hGet($hname,'paths');
-$res = $redis->hSet($hname, 'paths', $paths);
+$res = $redis->hSet($hname, 'day', $day);
+$res = $redis->hSet($hname, 'type', $type);
+$res = $redis->hSet($hname, 'no', $no);
 $res = $redis->hSet($hname, 'distance', $distance);
+$res = $redis->hSet($hname, 'paths', $paths);
+exit(1);
+
 
 
 
