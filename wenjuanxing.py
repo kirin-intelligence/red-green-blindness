0087a8c5db0256cb581fcc09dbdb68057a328316
wscjxky <619677632@qq.com>
2019-04-16 16:12:29 +0800

    i

---
+++ wenjuanxing.py
@@ -0,0 +1,13 @@
+from bs4 import BeautifulSoup
+with open('data.html','r',encoding='utf8')as f:
+    ls=f.read()
+    soup = BeautifulSoup(ls)
+    a=soup.find_all('div',class_='div_question')
+    for index,i in enumerate(a):
+        b = i.find('div', class_='div_title_question')
+        c = i.find_all('li')
+        print("%s、%s"%(index+1,b.text))
+        for j in c:
+            print('□'+j.text)
+
+
